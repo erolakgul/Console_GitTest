62ed291bd44ec2a8cb1997aedaa0ac608ed102b1
EROL AKGÜL <erolakgul88@gmail.com>
2022-12-12 11:08:42 +0300

    listed collection data

--- Console_GitTest/Program.cs
+++ Console_GitTest/Program.cs
@@ -5,3 +5,15 @@ for (int i = 0; i < 5; i++)
 {
     Console.WriteLine($"{i}. Erol\n");
 }
+
+
+List<int> listInt = new()
+{
+   1,
+   2,
+   3
+};
+foreach (var item in listInt)
+{
+    Console.WriteLine($"{item}.ci satır");
+}
