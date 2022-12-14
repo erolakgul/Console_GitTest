0a749465c8f0d6d21c63d030ae6ae12aff2b3b5b
EROL AKGÜL <erolakgul88@gmail.com>
2022-12-14 13:16:57 +0300

    added cache model

---
+++ Console_GitTest/CacheHelper/Cacher.cs
@@ -0,0 +1,16 @@
+﻿namespace Console_GitTest.CacheHelper
+{
+    public class Cacher
+    {
+
+        public Cacher()
+        {
+
+        }
+
+        public List<string> List()
+        {
+            return new List<string>();
+        }
+    }
+}
